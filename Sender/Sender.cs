d2dcb10c457f19fc5679b96980a8739dd4c8417b
Muskan <muskan.0135@gmail.com>
2020-09-16 16:28:07 +0530

    creating sender

---
+++ Sender/Sender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sender
+{
+    public class sender
+    {
+        public bool SendingMessage(String input)
+        {
+            if (input.Length != 0)
+            {
+                String output = input;
+                Console.WriteLine(output);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
