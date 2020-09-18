a43504dc2f1c947afed59672d890657953c23488
Muskan <muskan.0135@gmail.com>
2020-09-18 22:10:22 +0530

    updated  WriteCsvOutputFile.cs

--- Receiver/WriteCsvOutputFile.cs
+++ Receiver/WriteCsvOutputFile.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Text;
+
 /*using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +15,37 @@ namespace Receiver
 {
     class WriteCsvOutputFile
     {
+       readonly StringBuilder _output = new StringBuilder();
+       string path = @"c:\Result.csv";
+       private string _seperator = ",";
+       readonly string s = "E04";
+        public void WriteToCsvFromList(string heading, List<double> Input)
+        {
+            _output.AppendLine(heading);
+            _output.AppendLine();
+            foreach (var t in Input)
+            {
+                var k = t.ToString(s);
+                 _output.Append(string.Join(_seperator, k));
+            }
+
+            _output.AppendLine();
+        }
         public void WriteCsv(List<double> avgPerHourInDayList, List<double> avgPerDayInWeekList, string peakDayInMonth)
         {
             
+            WriteToCsvFromList("Average per hour in a day", avgPerHourInDayList);
+            _output.AppendLine();
+
+            WriteToCsvFromList("Average per day in a week", avgPerDayInWeekList);
+            _output.AppendLine();
+
+            _output.Append("Peak day in a month");
+            _output.Append(peakDayInMonth);
+
+            
+            File.WriteAllText(path, _output.ToString());
+
         }
     }
 }
